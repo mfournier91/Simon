4976146c3bc266029374e0e07dc5fe089294b3b6
Marc Fournier <marc.fournier91@gmail.com>
2016-07-09 16:16:13 -0400

    Added animation for lights using greensock library.

--- script.js
+++ script.js
@@ -2,13 +2,32 @@ $(document).on("ready", function(){
   var score = 0;
   var gameover = false;
   var lightOrder = [];
-  var divSelection;
+  var highScore = 0;
+  var newColor;
+  var divColor;
   //while (!gameover) {
     lightOrder.push(Math.floor(Math.random()*4));
     for(i=0;i<lightOrder.length;i++){
       console.log(lightOrder[i]);
-      divSelection = "#color" + lightOrder[i].toString();
-      console.log($(divSelection));
+      var divSelection = "#color" + lightOrder[i].toString();
+      divColor = $(divSelection).css("background");
+
+      if(lightOrder[i] == 0){
+        newColor = "#00ff57";
+      }
+      else if(lightOrder[i] == 1){
+        newColor = "#ff000a";
+      }
+      else if(lightOrder[i] == 2){
+        newColor = "#f2ff01";
+      }
+      else if(lightOrder[i] == 3){
+        newColor = "#0164ff";
+      }
+
+      console.log(divColor);
+      TweenMax.to(divSelection, 0.5, {backgroundColor:newColor, ease: Power0.easeNone});
+      TweenMax.from(divSelection, 0.5, {backgroundColor:newColor, ease: Power0.easeNone, delay:0.6});
     }
   //}
 });
