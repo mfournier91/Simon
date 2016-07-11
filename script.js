2a4cb936610097167fbc9f9d1bbebc2cba06c80b
Marc Fournier <marc.fournier91@gmail.com>
2016-07-11 10:10:45 -0400

    Added animations to onclick event listeners

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ $(document).on("ready", function(){
   var highScore = 0;
   var newColor;
   var divColor;
+  var greenButton = $("#color0");
+  var redButton = $("#color1");
+  var yellowButton = $("#color2");
+  var blueButton = $("#color3");
   //while (!gameover) {
     lightOrder.push(Math.floor(Math.random()*4));
     for(i=0;i<lightOrder.length;i++){
@@ -26,8 +30,32 @@ $(document).on("ready", function(){
       }
 
       console.log(divColor);
-      TweenMax.to(divSelection, 0.5, {backgroundColor:newColor, ease: Power0.easeNone});
-      TweenMax.from(divSelection, 0.5, {backgroundColor:newColor, ease: Power0.easeNone, delay:0.6});
+      (function() {
+        TweenMax.to(divSelection, 0.3, {backgroundColor:newColor, ease: Power0.easeNone});
+        TweenMax.from(divSelection, 0.3, {backgroundColor:newColor, ease: Power0.easeNone, delay:.4});
+      })();
     }
+    var choiceNum = 0;
+    var animateGreen = function(){
+      TweenMax.to(greenButton, 0.3, {backgroundColor:"#00ff57", ease: Power0.easeNone});
+      TweenMax.from(greenButton, 0.3, {backgroundColor:"#00ff57", ease: Power0.easeNone, delay:.4});
+    }
+    var animateRed = function(){
+      TweenMax.to(redButton, 0.3, {backgroundColor:"#ff000a", ease: Power0.easeNone});
+      TweenMax.from(redButton, 0.3, {backgroundColor:"#ff000a", ease: Power0.easeNone, delay:.4});
+    }
+    var animateYellow = function(){
+      TweenMax.to(yellowButton, 0.3, {backgroundColor:"#f2ff01", ease: Power0.easeNone});
+      TweenMax.from(yellowButton, 0.3, {backgroundColor:"#f2ff01", ease: Power0.easeNone, delay:.4});
+    }
+    var animateBlue = function(){
+      TweenMax.to(blueButton, 0.3, {backgroundColor:"#0164ff", ease: Power0.easeNone});
+      TweenMax.from(blueButton, 0.3, {backgroundColor:"#0164ff", ease: Power0.easeNone, delay:.4});
+    }
+
+    greenButton.on("click", animateGreen);
+    redButton.on("click", animateRed);
+    yellowButton.on("click", animateYellow);
+    blueButton.on("click", animateBlue);
   //}
 });
