119bd832841aa4909224398761850261d9845196
Marc Fournier <marc.fournier91@gmail.com>
2016-07-11 19:38:43 -0400

    Seperated animation function. Staggered setTimeout.

--- script.js
+++ script.js
@@ -12,63 +12,40 @@ $(document).on("ready", function(){
   var blueButton = $("#color3");
   var answers = [];
   var choiceNum = 0;
-  //while (!gameover) {
-  // function timeOutAnime(div){
-  //   var start = setTimeout(function(){
-  //     TweenMax.to(div, 0.3, {backgroundColor:newColor, ease: Power0.easeNone});
-  //     TweenMax.from(div, 0.3, {backgroundColor:newColor, ease: Power0.easeNone, delay:.4});
-  //   }, 1200)
-  //   start;
-  // }
 
-  function start(i) {
-    TweenMax.to(divSelection, 0.3, {backgroundColor:newColor, ease: Power0.easeNone});
-    console.log("Random number animation start "+i);
-    console.log($(divSelection).css("background"));
-  //};
-  //function stop(i) {
 
-    TweenMax.to(divSelection, 0.3, {backgroundColor:oldColor, ease: Power0.easeNone, delay:.4});
-    console.log("Random number animation end "+i);
-    console.log($(divSelection).css("background"));
+  function animate(animationObjectNum, n){
+    setTimeout( function(){
+      var animationObject = "#color" + animationObjectNum.toString();
+      if(animationObjectNum == 0){
+        newColor = "#00ff57";
+        oldColor = "#00a157";
+      }
+      else if(animationObjectNum == 1){
+        newColor = "#ff000a";
+        oldColor = "#a8000a";
+      }
+      else if(animationObjectNum == 2){
+        newColor = "#f2ff01";
+        oldColor = "#f2b001";
+      }
+      else if(animationObjectNum == 3){
+        newColor = "#0164ff";
+        oldColor = "#01649f";
+      }
+      TweenMax.to(animationObject, 0.3, {backgroundColor:newColor, ease: Power0.easeNone});
+      console.log("Start random animation");
+      TweenMax.to(animationObject, 0.3, {backgroundColor:oldColor, ease: Power0.easeNone, delay:.4});
+      console.log("End random animation");
+    }, (1400+n*500));
   }
 
   function animateRand(){
     lightOrder.push(Math.floor(Math.random()*4));
     for(i=0;i<lightOrder.length;i++){
-      console.log(lightOrder[i]);
-      divSelection = "#color" + lightOrder[i].toString(); //selects the div corresponding to the random number
-      //divColor = $(divSelection).css("background");
-
-      if(lightOrder[i] == 0){
-        newColor = "#00ff57";
-        oldColor = "#00a157";
-      }
-      else if(lightOrder[i] == 1){
-        newColor = "#ff000a";
-        oldColor = "#a8000a";
-      }
-      else if(lightOrder[i] == 2){
-        newColor = "#f2ff01";
-        oldColor = "#f2b001";
-      }
-      else if(lightOrder[i] == 3){
-        newColor = "#0164ff";
-        oldColor = "#01649f";
-      }
-      //console.log(newColor, oldColor);
-      //animation function for divs that uses the RNG value assigned to divSelection to animate the right color
-      // function start() {
-      //   TweenMax.to(divSelection, 0.3, {backgroundColor:newColor, ease: Power0.easeNone});
-      //   console.log("Random number animation start "+i);
-      // };
-      // function stop() {
-      //   TweenMax.to(divSelection, 0.3, {backgroundColor:oldColor, ease: Power0.easeNone, delay:.4});
-      //   console.log("Random number animation end "+i);
-      // }
-      setTimeout(start, 1200, i);
-      //setTimeout(stop, 300, i);
-      //timeOutAnime(divSelection);
+      console.log("Random Number : " + lightOrder[i], " Array index : "+i);
+      animate(lightOrder[i], i);
+    
     }
   }
   function nextRound(){
@@ -94,7 +71,7 @@ $(document).on("ready", function(){
       if (choiceNum == lightOrder.length){
         console.log("Do next round");
 
-        setTimeout(nextRound(), 1500);
+        setTimeout(nextRound, 1500);
       }
     }
     else if (answers[choiceNum - 1] == false) {
